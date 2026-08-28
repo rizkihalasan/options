d8b23b290a5433940107018b9af393a5b7f20174
Rizki Halasan <ekihalasan@gmail.com>
2026-08-28 18:14:48 -0400

    plot profit for options combination

--- payoff/initiator.py
+++ payoff/initiator.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import numpy as np
+from matplotlib.widgets import Slider, Button
 
 LONG = 'long'
 SHORT = 'short'
@@ -7,41 +9,67 @@ CALL = 'call'
 PUT = 'put'
 
 class Underlying:
-    def __init__(self, price=100, position=LONG, quantity=1):
-        self.price = price
+    def __init__(self, current_price=100, predicted_price=100, position=LONG, quantity=1):
+        self.current_price = current_price
+        self.predicted_price = predicted_price
         self.position = position
         self.quantity = quantity
 
 class Option:
-    def __init__(self, call_or_put, premium, strike_price, position, expiry, volatility, underlying, quantity=1):
+    def __init__(self, call_or_put, position, strike_price, premium, expiry, volatility, underlying, quantity=1):
         self.call_or_put = call_or_put
-        self.premium = premium
-        self.strike_price = strike_price
         self.position = position
+        self.strike_price = strike_price
+        self.premium = premium
         self.expiry = expiry
         self.volatility = volatility
         self.underlying = underlying
         self.quantity = quantity
 
 class Strategy:
-    def __init__(self, options=None, underlyings=None):
+    def __init__(self, options=None, underlying=None):
         self.options = options
-        self.underlyings = underlyings
+        self.underlying = underlying
     
     def calculate_profit(self):
-        spot_prices = np.linspace(80, 120, 100)
+        min_threshold = 0.80
+        max_threshold = 1.20
+        profit = 0
+        if not self.underlying:
+            spot_prices = np.linspace(80, 120, 100)
+            
+        else:
+            spot_prices = np.linspace(self.underlying.current_price * min_threshold, self.underlying.current_price * max_threshold, self.underlying.current_price)
+            if self.underlying.position == LONG:
+                profit += self.underlying.predicted_price - self.underlying.current_price
+            elif self.underlying.position == SHORT:
+                profit += self.underlying.current_price - self.underlying.predicted_price
+
         for option in self.options:
-            if option.call_or_put == CALL:
-                profit = np.maximum(-option.premium, spot_prices - option.strike_price)
-            else:
-                profit = np.maximum(-option.premium, option.strike_price - spot_prices)
+            if option.position == LONG:
+                if option.call_or_put == CALL:
+                    profit += (np.maximum(-option.premium, spot_prices - option.strike_price - option.premium) *  option.quantity)
+                elif option.call_or_put == PUT:
+                    profit += (np.maximum(-option.premium, option.strike_price - spot_prices - option.premium) *  option.quantity)
+            elif option.position == SHORT:
+                if option.call_or_put == CALL:
+                    profit += (-np.maximum(-option.premium, spot_prices - option.strike_price - option.premium) * option.quantity)
+                elif option.call_or_put == PUT:
+                    profit += (-np.maximum(-option.premium, option.strike_price - spot_prices - option.premium) * option.quantity)
 
         plt.figure(figsize=(10,6))
 
         plt.plot(spot_prices, profit, color="green", lw=2.5)
+        plt.axhline(0, color="black", linestyle="--", alpha=0.5)
 
-        
+        ax = plt.gca()
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+        plt.xlabel("Stock Price")
+        plt.ylabel("Profit")
+
         plt.show()
+        plt.ion()
 
 
 
--- payoff/main.py
+++ payoff/main.py
@@ -1,7 +1,15 @@
 from initiator import Underlying, Option, Strategy
 
-u = Underlying()
-put = Option('put', 2.5, 100, 'long', 'Mar 26', 1, u, quantity=1)
-s = Strategy([put], [u])
+u = Underlying(current_price=100, predicted_price=100, position='long')
+
+
+options1 = Option('call', 'short', 90, 9.35, 'Mar 26', 1, u, quantity=1)
+options2 = Option('call', 'long', 100, 2.7, 'Mar 26', 1, u, quantity=2)
+options3 = Option('put', 'short', 95, 1.55, 'Mar 26', 1, u, quantity=4)
+options4 = Option('put', 'long', 100, 3.7, 'Mar 26', 1, u, quantity=2)
+
+
+options = [options1, options2, options3, options4]
+s = Strategy(options)
 
 s.calculate_profit()
